c3e00c2b97e63ab7963c313c188cadd1c5c9eda3
CrazyKing <benedikt.grau@gmx.de>
2018-05-21 14:44:48 +0200

    Added Bullet
    Tower can shoot bullet and bullet detects hits

---
+++ TowDef/Assets/Scripts/Bullet.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+	private Transform target;
+
+	[SerializeField]
+	private float speed = 70f;
+	
+	public void Seek(Transform target) {
+		this.target = target;
+	}
+
+	void Update () {
+
+		if(target == null) {
+			Destroy(gameObject);
+			return;
+		}
+
+		Vector3 dir = target.position - transform.position;
+		float distThisFrame = speed * Time.deltaTime;
+
+		/* if(dir.magnitude <= distThisFrame) {
+			HitTarget();
+			return;
+		} */
+
+		transform.Translate(dir.normalized * distThisFrame, Space.World);
+		
+	}
+
+	private void HitTarget() {
+		Destroy(gameObject);
+	}
+
+	public void OnTriggerEnter(Collider collider) {
+		GameObject go = collider.gameObject;
+		if(go.CompareTag("Enemy")) {
+			Destroy(gameObject);
+			Debug.Log("HIT");
+		}
+	}
+
+}
---
+++ TowDef/Assets/Scripts/Tower.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour {
+
+	[Header("Attributes")]
+	[SerializeField]
+	private float range = 10f;
+
+	[SerializeField]
+	private float fireRate = 1f;
+
+	private float fireCountdown = 0f;
+
+
+	[Header("Unity Setup")] 
+	[SerializeField]
+	private Transform firePoint;
+
+	[SerializeField]
+	private GameObject partToRotate;
+
+	[SerializeField]
+	private float turnSpeed = 10f;
+
+	[SerializeField]
+	private GameObject bulletPrefab;
+
+	public LayerMask layerMask;
+
+
+
+	private List<GameObject> enemiesInRange = new List<GameObject>();
+
+	public void Awake() {
+		SphereCollider rangeTrigger = gameObject.AddComponent<SphereCollider>();
+		rangeTrigger.isTrigger = true;
+		rangeTrigger.radius = 10;		
+	}
+
+	public void Update() {
+
+		if(fireCountdown > 0f)
+			fireCountdown -= Time.deltaTime;
+
+
+		if(closestEnemy == null)
+			return;
+
+		Vector3 dir = closestEnemy.transform.position - partToRotate.transform.position;
+		Quaternion lookRotation = Quaternion.LookRotation(dir);
+		Vector3 rotation = Quaternion.Lerp(partToRotate.transform.rotation, lookRotation, Time.deltaTime * turnSpeed).eulerAngles;
+		partToRotate.transform.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+
+		// Draw a line for testing
+		Debug.DrawRay(firePoint.transform.position, closestEnemy.transform.position - firePoint.transform.position);
+
+
+		if(fireCountdown <= 0f) {
+			Shoot();
+			fireCountdown = 1f / fireRate;
+		}
+	}
+
+	private void Shoot() {
+		GameObject bulletGO = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+		Bullet bullet = bulletGO.GetComponent<Bullet>();
+
+		if(bullet != null) {
+			bullet.Seek(closestEnemy.transform);
+		}
+	}
+
+
+	// Triggers and functions for enemy detection
+	public void OnTriggerEnter(Collider collider) {
+		GameObject go = collider.gameObject;
+		if(go.CompareTag("Enemy")) {
+			AddEnemy(go);
+		}
+	}
+
+	public void OnTriggerExit(Collider collider) {
+		GameObject go = collider.gameObject;
+		if(go.CompareTag("Enemy")) {
+			RemoveEnemy(go);
+		}
+	}
+
+	private void AddEnemy(GameObject enemy) {
+		if(!enemiesInRange.Contains(enemy))
+			enemiesInRange.Add(enemy);
+	}
+
+	private void RemoveEnemy(GameObject enemy) {
+		if(enemiesInRange.Contains(enemy))
+			enemiesInRange.Remove(enemy);
+	}
+
+	GameObject closestEnemy {
+		get {
+			float closestDist = float.MaxValue;
+			GameObject closestEnemy = null;
+
+			foreach(GameObject enemy in enemiesInRange) {
+				float dist = (enemy.transform.position - transform.position).magnitude;
+				Vector3 dir = enemy.transform.position - firePoint.transform.position;
+				if(dist < closestDist) {
+					RaycastHit hit;
+					// This part has problem with the tower model
+					if(Physics.Raycast(firePoint.transform.position, dir, out hit, Mathf.Infinity, ~layerMask)) {
+						if(hit.collider.gameObject == enemy) {
+							closestEnemy = enemy;
+							closestDist = dist;
+						}
+					}
+				}
+			}
+
+			return closestEnemy;
+		}
+	}
+
+
+	// Draw method for range indicator
+	public void OnDrawGizmosSelected() {
+		Gizmos.color = Color.green;
+		Gizmos.DrawWireSphere(transform.position, range);
+	}
+
+}
