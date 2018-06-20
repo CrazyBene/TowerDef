666373bd1fe58a4ca3f7e980060d0bc2182f50ec
CrazyKing <benedikt.grau@gmx.de>
2018-06-20 16:29:46 +0200

    Improved Spawning System + Added TextMeshPro
    LevelManager now tells the spawners to spawn their enemies, when going into Spawning Phase.
    Phases now work almost properly. Still need to detect, when all enemies are dead or the nexus is destroyed

---
+++ TowDef/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+	[SerializeField]
+	private Spawner[] spawners;
+
+	[SerializeField]
+	private TextMeshProUGUI waveCounter;
+
+	[SerializeField]
+	private int maxWaves = 1;
+
+	private int currentWave = 0;
+	
+	// At the moment for debugging
+	public TextMeshProUGUI phaseText;
+	
+	private LevelPhase levelPhase = LevelPhase.BuildPhase;
+
+	private void Awake() {
+		UpdateUI();
+	}
+
+
+	private void Update () {
+		
+		UpdateUI();
+		
+		switch(levelPhase) {
+			case LevelPhase.BuildPhase: 
+				UpdateBuildPhase();
+				break;
+			case LevelPhase.WavePhase: 
+				UpdateWavePhase();
+				break;
+			case LevelPhase.EndPhase: 
+				UpdateEndPhase();
+				break;
+		}
+
+	}
+
+	private void UpdateBuildPhase() {
+		// If the player presses 'G' the build phase ends and the wave starts spawning
+		if(Input.GetKeyDown(KeyCode.G)) {
+			levelPhase = LevelPhase.WavePhase;
+			currentWave++;
+			foreach(Spawner spawner in spawners) {
+				StartCoroutine(spawner.SpawnWave(currentWave));
+			}
+		}
+	}
+
+	private float timeBetweenSpawnsInSec = 2f;
+	private float nextSpawn = 2f;
+	private float spawnTime = 20f;
+	private void UpdateWavePhase() {
+		// TODO: Still need to detect if all enemies are destroyed
+		if(!spawningEnemies) {
+			if(currentWave == maxWaves) 
+				levelPhase = LevelPhase.EndPhase;
+			else
+				levelPhase = LevelPhase.BuildPhase;
+		}
+
+		// If the nexus gets destroyed go to the endphase
+	}
+
+	private void UpdateEndPhase() {
+		// If the player wants to end this level he can proceed to the menu?
+	}
+
+	
+	private bool spawningEnemies {
+		get {
+			foreach(Spawner spawner in spawners) {
+				if(spawner.Spawning)
+					return true;
+			}
+			return false;
+		}
+	}
+
+	private void UpdateUI() {
+		waveCounter.text = "Wave " + currentWave + "/" + maxWaves;
+
+		switch(levelPhase) {
+			case LevelPhase.BuildPhase: 
+				phaseText.text = "Build";
+				break;
+			case LevelPhase.WavePhase: 
+				phaseText.text = "Wave";
+				break;
+			case LevelPhase.EndPhase: 
+				phaseText.text = "End";
+				break;
+		}
+	}
+
+}
+
+enum LevelPhase {
+	BuildPhase,
+	WavePhase,
+	EndPhase
+}
--- TowDef/Assets/Scripts/Spawner.cs
+++ TowDef/Assets/Scripts/Spawner.cs
@@ -11,25 +11,59 @@ public class Spawner : MonoBehaviour {
 	[SerializeField]
 	private Transform nexus;
 
+	
+	// the list for one wave, maybe change this to an external file?
+	public List<EnemyWithTime> enemyToSpawn = new List<EnemyWithTime>();
 
-	private float timeBetweenSpawnsInSec = 5f;
+
+    public bool Spawning  {
+		get {
+			return this;
+		}
+
+		private set {
+			Spawning = value;
+		}
+	}
+
+
+    private float timeBetweenSpawnsInSec = 5f;
 	private float nextSpawn = 5f;
 
-	private void Update() {
-		nextSpawn -= Time.deltaTime;
-
-		if(nextSpawn <= 0) {
-			SpawnEnemy();
-			nextSpawn = timeBetweenSpawnsInSec;
-		}
+    private void Update() {
 
 	}
 
-	private void SpawnEnemy() {
+	public void SpawnEnemy() {
 		GameObject enemyGameObject = Instantiate(enemyPrefab, spawnPoint.position, transform.rotation);
 		EnemyMovement enemyMovement = enemyGameObject.GetComponent<EnemyMovement>();
 		enemyMovement.SetDestination(nexus);
 	}
 
+	public IEnumerator SpawnWave(int waveNumber) {
+		Spawning = true;
+		foreach(EnemyWithTime e in enemyToSpawn) {
+			yield return new WaitForSeconds(e.time);
+			SpawnEnemy(e.enemy);
+		}
 
+		Spawning = false;
+		yield return new WaitForEndOfFrame();
+	}
+
+	private void SpawnEnemy(GameObject thisEnemyPrefab) {
+		GameObject enemyGameObject = Instantiate(thisEnemyPrefab, spawnPoint.position, transform.rotation);
+		EnemyMovement enemyMovement = enemyGameObject.GetComponent<EnemyMovement>();
+		enemyMovement.SetDestination(nexus);
+	}
+
+
+}
+
+
+
+[System.Serializable]
+public class EnemyWithTime {
+	public GameObject enemy;
+	public float time;
 }
