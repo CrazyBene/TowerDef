b5e52cc386f5063a765671bea6645bfe09e1cc04
CrazyKing <benedikt.grau@gmx.de>
2018-06-24 12:34:27 +0200

    Added UI Healthbar for Nexus
    Added a healthbar to the nexus so the player can see it

---
+++ TowDef/Assets/Scripts/Nexus.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Nexus : MonoBehaviour {
+
+	[SerializeField]
+	public float health = 1000;
+
+	public float currentHealth;
+
+	private void Awake() {
+		currentHealth = health;
+	}
+
+	public void TakeDamage(float damage) {
+		currentHealth -= damage;
+	}
+
+	public bool Destroyed {
+		get {
+			return currentHealth <= 0;
+		}
+	}
+
+}
---
+++ TowDef/Assets/Scripts/NexusUI.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Nexus))]
+public class NexusUI : MonoBehaviour {
+
+	private Nexus nexus;
+
+	private Transform player;
+
+	[SerializeField]
+	private Image healthbarRotation;
+
+	[SerializeField]
+	private Image healthbar;
+	
+	private void Awake() {
+		nexus = GetComponent<Nexus>();
+
+		player = GameObject.FindGameObjectWithTag("Player").transform;
+	}
+
+	private void Update () {
+		var procent = nexus.currentHealth / nexus.health;
+		healthbar.fillAmount = procent;
+
+		Vector3 dir = player.position - transform.position;
+		dir.y = 0;
+		healthbarRotation.transform.rotation = Quaternion.LookRotation(dir);
+	}
+
+}
